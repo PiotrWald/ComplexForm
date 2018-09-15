726e204a2fed4bf4b1f1dce63ff2afda527ff596
Piotr Wald <valdpiotr@gmail.com>
2018-09-15 19:48:05 +0200

    refactored user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,16 +17,3 @@ class User < ApplicationRecord
     false
   end
 end
-
-
-# ● Pole First name​jest obowiązkowe
-# ● Pole First name​nie może zawierać więcej niż 100 znaków
-# ● Pole Last name​jest obowiązkowe
-# ● Pole Last name​nie może zawierać więcej niż 100 znaków
-# ● Pole Email address​jest obowiązkowe
-# ● Pole Email address​musi zawierać prawidłowy adres e-mail
-# ● Pole Date of birth​jest opcjonalne
-# ● Pole Date of birth​musi zawierać prawidłową datę (jeżeli je wypełniono)
-# ● Pole Date of birth​musi zawierać datę w przeszłości (jeżeli je wypełniono)
-# ● Pole Phone number​jest opcjonalne
-# ● Pole Phone number​musi zawierać prawidłowy numer telefonu (jeżeli je wypełniono)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,78 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before(:each) do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'creation' do
     context 'with valid params' do
       it 'should pass validaton' do
-        expect { FactoryBot.create(:user) }.to_not raise_error
+        expect { @user.save }.to_not raise_error
       end
 
       it 'should pass validation when birth_date is empty' do
-        user = FactoryBot.build(:user)
-        user.birth_date = nil
-        expect { user.save! }.to_not raise_error
+        @user.birth_date = nil
+        expect { @user.save! }.to_not raise_error
       end
 
       it 'should pass validation when phone_number is empty' do
-        user = FactoryBot.build(:user)
-        user.phone_number = nil
-        expect { user.save! }.to_not raise_error
+        @user.phone_number = nil
+        expect { @user.save! }.to_not raise_error
       end  
     end
 
     context 'with invalid params' do
       it 'should fail validaton when first_name is empty' do
-        user = FactoryBot.build(:user)
-        user.first_name = nil
-        expect { user.save! }.to raise_error( ActiveRecord::RecordInvalid )
+        @user.first_name = nil
+        expect { @user.save! }.to raise_error( ActiveRecord::RecordInvalid )
       end
 
       it 'should fail validaton when first_name has more than 100 chars' do
-        user = FactoryBot.build(:user)
-        user.first_name = Array.new(101).map!(&:to_i).join
-        expect { user.save! }.to raise_error( ActiveRecord::RecordInvalid )
+        @user.first_name = Array.new(101).map!(&:to_i).join
+        expect { @user.save! }.to raise_error( ActiveRecord::RecordInvalid )
       end
 
       it 'should fail validaton when last_name is empty' do
-        user = FactoryBot.build(:user)
-        user.last_name = nil
-        expect { user.save! }.to raise_error( ActiveRecord::RecordInvalid )
+        @user.last_name = nil
+        expect { @user.save! }.to raise_error( ActiveRecord::RecordInvalid )
       end
 
       it 'should fail validaton when last_name has more than 100 chars' do
-        user = FactoryBot.build(:user)
-        user.last_name = Array.new(101).map!(&:to_i).join
-        expect { user.save! }.to raise_error( ActiveRecord::RecordInvalid )
+        @user.last_name = Array.new(101).map!(&:to_i).join
+        expect { @user.save! }.to raise_error( ActiveRecord::RecordInvalid )
       end
 
       it 'should fail validaton when email is empty' do
-        user = FactoryBot.build(:user)
-        user.email = nil
-        expect { user.save! }.to raise_error( ActiveRecord::RecordInvalid )
+        @user.email = nil
+        expect { @user.save! }.to raise_error( ActiveRecord::RecordInvalid )
       end
 
       it 'should fail validaton when email is incorrect #1 case' do
-        user = FactoryBot.build(:user)
-        user.email = '@test.com'
-        expect { user.save! }.to raise_error( ActiveRecord::RecordInvalid )
+        @user.email = '@test.com'
+        expect { @user.save! }.to raise_error( ActiveRecord::RecordInvalid )
       end
 
       it 'should fail validaton when email is incorrect #2 case' do
-        user = FactoryBot.build(:user)
-        user.email = 'example@'
-        expect { user.save! }.to raise_error( ActiveRecord::RecordInvalid )
+        @user.email = 'example@'
+        expect { @user.save! }.to raise_error( ActiveRecord::RecordInvalid )
       end
 
       it 'should fail validation when birth_date isnt in the past' do
-        user = FactoryBot.build(:user)
-        user.birth_date = Date.tomorrow
-        expect { user.save! }.to raise_error( ActiveRecord::RecordInvalid )       
+        @user.birth_date = Date.tomorrow
+        expect { @user.save! }.to raise_error( ActiveRecord::RecordInvalid )       
       end
 
       it 'should fails validation when phone_number isnt in correct format' do
-        user = FactoryBot.build(:user)
-        user.phone_number = '22-2-13-9201213'
-        expect { user.save! }.to raise_error( ActiveRecord::RecordInvalid )          
+        @user.phone_number = '22-2-13-9201213'
+        expect { @user.save! }.to raise_error( ActiveRecord::RecordInvalid )          
       end
     end
   end
